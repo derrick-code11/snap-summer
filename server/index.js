2620df45a581610721d276d58f81e25d5d07bc34
derrick-code11 <agyemangderrick777@gmail.com>
2024-06-30 00:29:05 -0400

    testing server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import postRoutes from "./routes/posts.js"
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+app.use(cors());
 
 // Parse application/json
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -11,16 +13,14 @@ app.use(express.json({ limit: "30mb", extended: true }));
 // Parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors());
+const CONNECTION_URL =
+  "";
 
-const CONNECTION_URL = "";
-  
 const PORT = process.env.PORT || 5000;
+
+app.use("/posts", postRoutes);
 
 mongoose
   .connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
   .catch((error) => console.log(error.message));
-
-app.use('/posts', postRoutes)
-
