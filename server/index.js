b9eaec1e8cb1d7de1ba64a4e9eb314f7318c6f7d
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-03 19:55:29 -0400

    modified code for deployment

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
 
 app.use(cors());
+dotenv.config();
 
 // Parse application/json
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -13,14 +15,15 @@ app.use(express.json({ limit: "30mb", extended: true }));
 // Parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-const CONNECTION_URL =
-  "";
-
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to SnapSummer API");
+});
 
 app.use("/posts", postRoutes);
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
   .catch((error) => console.log(error.message));
