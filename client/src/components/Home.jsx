8954c1cee464f889f5c2294030efd47886b17c53
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-04 00:38:09 -0400

    added logic for login/signup

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,18 @@
 import { Container, Typography, Button, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleSignup = () => {
+    navigate("/signup");
+  };
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
   return (
-    <div className="home-background">
+    <div className="background home">
       <Container
         maxWidth="sm"
         sx={{
@@ -32,9 +42,16 @@ const Home = () => {
         </Typography>
         <Box sx={{ display: "flex", gap: 2, mt: 3 }}>
           <Button
-            sx={{ backgroundColor: "green", borderRadius: "30px" }}
+            sx={{
+              backgroundColor: "green",
+              borderRadius: "30px",
+              "&:hover": {
+                backgroundColor: "darkgreen",
+              },
+            }}
             variant="contained"
             size="large"
+            onClick={handleLogin}
           >
             Login
           </Button>
@@ -42,6 +59,7 @@ const Home = () => {
             sx={{ backgroundColor: "#4888F4", borderRadius: "30px" }}
             variant="contained"
             size="large"
+            onClick={handleSignup}
           >
             Sign Up
           </Button>
