2bf8046d386b19928f3cee3a241db588c997c703
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-04 00:38:47 -0400

    added routes for different app pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,32 @@
-import { Container} from "@mui/material";
-import BgImg from "./components/Background.jsx";
-import Navbar from "./components/Navbar.jsx";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
-import Feed from "./components/Feed.jsx";
+import SignUp from "./components/SignUp.jsx";
+import Login from "./components/Login.jsx";
 import "./index.css";
 
 function App() {
   return (
     <>
-    <Home></Home>
-      {/* <BgImg></BgImg>
-      <Container maxWidth="lg">
-        <Navbar></Navbar>
-        <Feed></Feed>
-      </Container> */}
+      <Router>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+      ;
     </>
   );
 }
 
 export default App;
+
+// {
+//   /* <BgImg></BgImg>
+//       <Container maxWidth="lg">
+//         <Navbar></Navbar>
+//         <Feed></Feed>
+//       </Container> */
+// }
