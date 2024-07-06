ffc1a9c9e939ae5e1d6478ee542326834b606a37
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-06 01:34:27 -0400

    added routes to new components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import SignUp from "./components/SignUp.jsx";
 import Login from "./components/Login.jsx";
+import ResetPassword from "./components/ResetPassword.jsx";
+import Dashbord from "./components/Dashbord.jsx";
 import "./index.css";
 
 function App() {
@@ -11,12 +13,13 @@ function App() {
       <Router>
         <CssBaseline />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/forget-password" element={<ResetPassword />} />
+          <Route path="/dashboard" element={<Dashbord />} />
         </Routes>
       </Router>
-      ;
     </>
   );
 }
@@ -24,9 +27,9 @@ function App() {
 export default App;
 
 // {
-//   /* <BgImg></BgImg>
+//    <BgImg></BgImg>
 //       <Container maxWidth="lg">
 //         <Navbar></Navbar>
 //         <Feed></Feed>
-//       </Container> */
+//       </Container>
 // }
