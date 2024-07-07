e5011f662452df1ec3e7d9adae1aa0bd17819609
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-06 22:00:16 -0400

    created this component for google auth

---
+++ client/src/components/GoogleLoginButton.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+// eslint-disable-next-line react/prop-types
+const GoogleLoginButton = ({ buttonText }) => {
+  const navigate = useNavigate();
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+  const onSuccess = async (response) => {
+    try {
+      const result = await axios.post(
+        "https://snap-summer-d53ae387f53b.herokuapp.com/api/v1/auth/google",
+        {
+          token: response.credential,
+        }
+      );
+      localStorage.setItem("token", result.data.token);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error("Google sign-in error:", error);
+    }
+  };
+
+  const onError = (response) => {
+    console.error("Google sign-in failed:", response);
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin onSuccess={onSuccess} onError={onError} useOneTap />
+    </GoogleOAuthProvider>
+  );
+};
+
+export default GoogleLoginButton;
