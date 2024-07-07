4c52b5eabf23476109f2e672a6b1959c28e2dc64
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-06 22:04:20 -0400

    added authorization headers

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,37 @@
 import axios from "axios";
 const url = "https://snap-summer-d53ae387f53b.herokuapp.com/api/v1/posts";
+const token = localStorage.getItem("token");
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatedPost) => {
-  axios.patch(`${url}/${id}`, updatedPost);
-};
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => {
-  axios.patch(`${url}/${id}/likePost`);
-};
+export const fetchPosts = () =>
+  axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+export const createPost = (newPost) =>
+  axios.post(url, newPost, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`${url}/${id}`, updatedPost, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export const deletePost = (id) =>
+  axios.delete(`${url}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export const likePost = (id) =>
+  axios.patch(`${url}/${id}/likePost`, null, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
