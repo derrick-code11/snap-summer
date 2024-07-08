c5b1a791ed853dd1aa4fc2277b51ee91b8d955da
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-07 21:05:20 -0400

    no visible changes made

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import postsReducer from './posts';
+import authReducer from './auth'
 
 const rootReducer = combineReducers({
   posts: postsReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
