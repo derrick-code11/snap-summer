84db780d3748d5fb42313f16be5c8252d35c3bfe
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-07 21:07:34 -0400

    modified component for like/edit functionality

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,36 +1,51 @@
 /* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import {
   Card,
   CardActions,
   CardContent,
   CardMedia,
   Button,
+  Box,
   Typography,
 } from "@mui/material";
-import { useState, useEffect } from "react";
 import { deletePost, likePost } from "../reducers/posts";
-import { useDispatch } from "react-redux";
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import DeleteIcon from "@mui/icons-material/Delete";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import moment from "moment";
 
 const Post = ({ post, setCurrentId, setPostDeleted }) => {
   const dispatch = useDispatch();
-  const [localLikeCount, setLocalLikeCount] = useState(post.likeCount);
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.auth.user);
+  const [localLikeCount, setLocalLikeCount] = useState(post.likes.length);
 
   useEffect(() => {
-    setLocalLikeCount(post.likeCount);
-  }, [post.likeCount]);
+    setLocalLikeCount(post.likes.length);
+  }, [post.likes, user?._id]);
 
-  const handleDelete = async () => {
-    await dispatch(deletePost(post._id));
+  const handleDelete = () => {
+    dispatch(deletePost(post._id));
     setPostDeleted(true);
   };
 
   const handleLikeCount = async () => {
-    await dispatch(likePost(post._id));
-    setLocalLikeCount((prev) => prev + 1);
+    const updatedPost = await dispatch(likePost(post._id)).unwrap();
+    setLocalLikeCount(updatedPost.likes.length);
+  };
+
+  const handleCardClick = () => {
+    navigate(`/posts/${post._id}`);
+  };
+
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    }
+    return text;
   };
 
   return (
@@ -44,74 +59,79 @@ const Post = ({ post, setCurrentId, setPostDeleted }) => {
         position: "relative",
       }}
     >
-      <CardMedia
-        sx={{
-          height: 0,
-          paddingTop: "56.25%",
-          backgroundColor: "rgba(0, 0, 0, 0.5)",
-          backgroundBlendMode: "darken",
-        }}
-        image={post.selectedFile}
-        title={post.title}
-      ></CardMedia>
-      <div
-        style={{
-          position: "absolute",
-          top: "20px",
-          left: "15px",
-          color: "white",
-        }}
-      >
-        <Typography variant="h6">{post.creator}</Typography>
-        <Typography variant="body2">
-          {moment(post.createdAt).fromNow()}
-        </Typography>
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "20px",
-          right: "20px",
-          color: "white",
-        }}
-      >
-        <Button
-          style={{ color: "white" }}
-          size="small"
-          onClick={() => {
-            setCurrentId(post._id);
+      <Box onClick={handleCardClick} sx={{ cursor: "pointer" }}>
+        <CardMedia
+          sx={{
+            height: 0,
+            paddingTop: "56.25%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            backgroundBlendMode: "darken",
+          }}
+          image={post.selectedFile}
+          title={post.title}
+        ></CardMedia>
+        <div
+          style={{
+            position: "absolute",
+            top: "20px",
+            left: "15px",
+            color: "white",
           }}
         >
-          <MoreHorizIcon fontSize="medium"></MoreHorizIcon>
-        </Button>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          margin: "20px",
-        }}
-      >
-        <Typography variant="body2" sx={{color: "#3270C0"}}>
-          {post.tags.map((tag) => `#${tag} `)}
+          <Typography variant="h6">{post.creator}</Typography>
+          <Typography variant="body2">
+            {moment(post.createdAt).fromNow()}
+          </Typography>
+        </div>
+        {user?.firstName === post.creator && (
+          <div
+            style={{
+              position: "absolute",
+              top: "20px",
+              right: "20px",
+              color: "white",
+            }}
+          >
+            <Button
+              style={{ color: "white" }}
+              size="small"
+              onClick={() => {
+                setCurrentId(post._id);
+              }}
+            >
+              <MoreHorizIcon fontSize="medium"></MoreHorizIcon>
+            </Button>
+          </div>
+        )}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            margin: "20px",
+          }}
+        >
+          <Typography variant="body2" sx={{ color: "#3270C0" }}>
+            {post.tags.map((tag) => `#${tag} `)}
+          </Typography>
+        </div>
+        <Typography
+          sx={{
+            padding: "0 16px",
+          }}
+          gutterBottom
+          variant="h5"
+          component="h2"
+        >
+          {post.title}
         </Typography>
-      </div>
-      <Typography
-        sx={{
-          padding: "0 16px",
-        }}
-        gutterBottom
-        variant="h5"
-        component="h2"
-      >
-        {post.title}
-      </Typography>
 
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {post.message}
-        </Typography>
-      </CardContent>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {truncateText(post.message, 150)}
+          </Typography>
+        </CardContent>
+      </Box>
+
       <CardActions
         sx={{
           padding: "0 16px 8px 16px",
@@ -126,9 +146,11 @@ const Post = ({ post, setCurrentId, setPostDeleted }) => {
           ></FavoriteIcon>
           {localLikeCount}
         </Button>
-        <Button size="small" color="primary" onClick={handleDelete}>
-          <DeleteIcon fontSize="small"></DeleteIcon>
-        </Button>
+        {user?.firstName === post.creator && (
+          <Button size="small" color="primary" onClick={handleDelete}>
+            <DeleteIcon fontSize="small"></DeleteIcon>
+          </Button>
+        )}
       </CardActions>
     </Card>
   );
