913c5e17b745d59c8efa6a60368575ddbe498533
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-07 20:57:46 -0400

    stored token to allow user to access protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { jwtDecode } from "jwt-decode";
+import { setUser } from "./reducers/auth.js";
 import Home from "./components/Home.jsx";
 import SignUp from "./components/SignUp.jsx";
 import Login from "./components/Login.jsx";
 import ResetPassword from "./components/ResetPassword.jsx";
 import Dashbord from "./components/Dashbord.jsx";
+import PostDetails from "./components/PostDetails.jsx";
 import "./index.css";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const user = {
+        id: decodedToken.id,
+        firstName: decodedToken.firstName,
+      };
+      dispatch(setUser(user));
+    }
+  }, [dispatch]);
   return (
     <>
       <Router>
@@ -18,6 +35,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/forget-password" element={<ResetPassword />} />
           <Route path="/dashboard" element={<Dashbord />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
         </Routes>
       </Router>
     </>
@@ -25,11 +43,3 @@ function App() {
 }
 
 export default App;
-
-// {
-//    <BgImg></BgImg>
-//       <Container maxWidth="lg">
-//         <Navbar></Navbar>
-//         <Feed></Feed>
-//       </Container>
-// }
