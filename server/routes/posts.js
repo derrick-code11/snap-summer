145f563a1f351e30ef09cf40f6b637d0413c43f9
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-13 04:29:42 -0400

    added routes for add/delete comment feature

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,15 +5,21 @@ import {
   updateOldPost,
   deletePost,
   likePost,
+  addComment,
+  deleteComment,
+  getPostById,
 } from "../controllers/postsController.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/", auth, getPosts);
+router.get('/:id', auth, getPostById);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updateOldPost);
 router.delete("/:id", auth, deletePost);
 router.patch("/:id/likePost", auth, likePost);
+router.post('/:id/comment', auth, addComment);
+router.delete('/:postId/comment/:commentId', auth, deleteComment);
 
 export default router;
