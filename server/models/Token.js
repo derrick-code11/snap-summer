a6ce9668a085f1e96519e7102534e8de301bda18
derrick-code11 <agyemangderrick777@gmail.com>
2024-07-13 16:23:17 -0400

    added Token schema to track user password reset

---
+++ server/models/Token.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const tokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 1800,
+  },
+});
+
+export default mongoose.model("Token", tokenSchema);
